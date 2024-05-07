8cb3b29740bfe6946ada6e798a170f8e2e7bb350
Paul Schaub <vanitasvitae@fsfe.org>
2024-05-07 10:41:22 +0200

    Compare key encoding

--- pg/src/test/java/org/bouncycastle/bcpg/test/Ed25519KeyTest.java
+++ pg/src/test/java/org/bouncycastle/bcpg/test/Ed25519KeyTest.java
@@ -6,25 +6,46 @@ import org.bouncycastle.openpgp.PGPPublicKey;
 import org.bouncycastle.openpgp.PGPPublicKeyRing;
 import org.bouncycastle.util.encoders.Hex;
 
-public class Ed25519KeyTest extends AbstractPacketTest {
+import java.io.IOException;
+
+public class Ed25519KeyTest
+        extends AbstractPacketTest
+{
     @Override
-    public String getName() {
+    public String getName()
+    {
         return "Ed25519KeyTest";
     }
 
     @Override
-    public void performTest() throws Exception {
-        // primary key from https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-sample-v6-certificate-trans
-        PGPObjectFactory objFactory = hexObjectFactory("c62a0663877fe31b00000020f94da7bb48d60a61e567706a6587d0331999bb9d891a08242ead84543df895a3");
-        PGPPublicKeyRing ring = (PGPPublicKeyRing) objFactory.nextObject();
-        PGPPublicKey k = ring.getPublicKey();
-
-        isEquals("version mismatch", 6, k.getVersion());
-        isEquals("Public key algorithm mismatch", PublicKeyAlgorithmTags.Ed25519, k.getAlgorithm());
-        isEncodingEqual("Fingerprint mismatch", Hex.decode("CB186C4F0609A697E4D52DFA6C722B0C1F1E27C18A56708F6525EC27BAD9ACC9"), k.getFingerprint());
+    public void performTest()
+            throws Exception
+    {
+        testParseEd25519PGPPublicKey();
     }
 
-    public static void main(String[] args) {
+    private void testParseEd25519PGPPublicKey()
+            throws IOException
+    {
+        // primary key from https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-sample-v6-certificate-trans
+        PGPObjectFactory objFactory = hexObjectFactory(
+                "c62a0663877fe31b00000020f94da7bb48d60a61e567706a6587d0331999bb9d891a08242ead84543df895a3");
+        PGPPublicKeyRing cert = (PGPPublicKeyRing) objFactory.nextObject();
+        PGPPublicKey key = cert.getPublicKey();
+
+        isEquals("version mismatch", 6, key.getVersion());
+        isEquals("creation time mismatch",
+                Long.valueOf("63877fe3", 16) * 1000, key.getCreationTime().getTime());
+        isEquals("Public key algorithm mismatch", PublicKeyAlgorithmTags.Ed25519, key.getAlgorithm());
+        isEncodingEqual("ed25519 public key mismatch",
+                Hex.decode("f94da7bb48d60a61e567706a6587d0331999bb9d891a08242ead84543df895a3"),
+                key.getPublicKeyPacket().getKey().getEncoded());
+        isEncodingEqual("Fingerprint mismatch",
+                Hex.decode("CB186C4F0609A697E4D52DFA6C722B0C1F1E27C18A56708F6525EC27BAD9ACC9"), key.getFingerprint());
+    }
+
+    public static void main(String[] args)
+    {
         runTest(new Ed25519KeyTest());
     }
 }
--- pg/src/test/java/org/bouncycastle/bcpg/test/X25519KeyTest.java
+++ pg/src/test/java/org/bouncycastle/bcpg/test/X25519KeyTest.java
@@ -8,31 +8,44 @@ import org.bouncycastle.util.encoders.Hex;
 
 import java.io.IOException;
 
-public class X25519KeyTest extends AbstractPacketTest {
-
+public class X25519KeyTest
+        extends AbstractPacketTest
+{
     @Override
-    public String getName() {
+    public String getName()
+    {
         return "X25519KeyTest";
     }
 
     @Override
-    public void performTest() throws Exception {
+    public void performTest()
+            throws Exception
+    {
         testParseX25519PGPPublicKey();
     }
 
-    private void testParseX25519PGPPublicKey() throws IOException {
+    private void testParseX25519PGPPublicKey()
+            throws IOException
+    {
         // subkey from https://www.ietf.org/archive/id/draft-ietf-openpgp-crypto-refresh-13.html#name-sample-v6-certificate-trans
-        PGPObjectFactory objFactory = hexObjectFactory("c62a0663877fe319000000208693248367f9e5015db922f8f48095dda784987f2d5985b12fbad16caf5e4435");
-        PGPPublicKeyRing ring = (PGPPublicKeyRing) objFactory.nextObject();
-        PGPPublicKey key = ring.getPublicKey();
+        PGPObjectFactory objFactory = hexObjectFactory(
+                "c62a0663877fe319000000208693248367f9e5015db922f8f48095dda784987f2d5985b12fbad16caf5e4435");
+        PGPPublicKeyRing cert = (PGPPublicKeyRing) objFactory.nextObject();
+        PGPPublicKey key = cert.getPublicKey();
 
         isEquals("version mismatch", 6, key.getVersion());
+        isEquals("creation time mismatch",
+                Long.valueOf("63877fe3", 16) * 1000, key.getCreationTime().getTime());
         isEquals("algorithm mismatch", PublicKeyAlgorithmTags.X25519, key.getAlgorithm());
+        isEncodingEqual("x25519 key mismatch",
+                Hex.decode("8693248367f9e5015db922f8f48095dda784987f2d5985b12fbad16caf5e4435"),
+                key.getPublicKeyPacket().getKey().getEncoded());
         isEncodingEqual("Fingerprint mismatch",
                 Hex.decode("12C83F1E706F6308FE151A417743A1F033790E93E9978488D1DB378DA9930885"), key.getFingerprint());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         runTest(new X25519KeyTest());
     }
 }
