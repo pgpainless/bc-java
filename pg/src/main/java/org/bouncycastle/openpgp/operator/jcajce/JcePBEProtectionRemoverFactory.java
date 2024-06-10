01054ffaab142b5f494802241dd9ed6d39bb47d7
Paul Schaub <vanitasvitae@fsfe.org>
2024-06-10 15:33:25 +0200

    Implement secret key decryption using AEAD and Argon2

--- pg/src/main/java/org/bouncycastle/openpgp/operator/jcajce/JcePBEProtectionRemoverFactory.java
+++ pg/src/main/java/org/bouncycastle/openpgp/operator/jcajce/JcePBEProtectionRemoverFactory.java
@@ -7,7 +7,6 @@ import java.security.Provider;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.spec.IvParameterSpec;
 
 import org.bouncycastle.jcajce.spec.AEADParameterSpec;
 import org.bouncycastle.jcajce.util.DefaultJcaJceHelper;
@@ -29,17 +28,20 @@ public class JcePBEProtectionRemoverFactory
     private PGPDigestCalculatorProvider calculatorProvider;
 
     private JcaPGPDigestCalculatorProviderBuilder calculatorProviderBuilder;
+    private JcePBESecretKeyDecryptorBuilder decryptorBuilder;
 
     public JcePBEProtectionRemoverFactory(char[] passPhrase)
     {
         this.passPhrase = passPhrase;
         this.calculatorProviderBuilder = new JcaPGPDigestCalculatorProviderBuilder();
+        this.decryptorBuilder = new JcePBESecretKeyDecryptorBuilder(calculatorProvider);
     }
 
     public JcePBEProtectionRemoverFactory(char[] passPhrase, PGPDigestCalculatorProvider calculatorProvider)
     {
         this.passPhrase = passPhrase;
         this.calculatorProvider = calculatorProvider;
+        this.decryptorBuilder = new JcePBESecretKeyDecryptorBuilder(calculatorProvider);
     }
 
     public JcePBEProtectionRemoverFactory setProvider(Provider provider)
@@ -50,7 +52,7 @@ public class JcePBEProtectionRemoverFactory
         {
             calculatorProviderBuilder.setProvider(provider);
         }
-
+        decryptorBuilder.setProvider(provider);
         return this;
     }
 
@@ -62,7 +64,7 @@ public class JcePBEProtectionRemoverFactory
         {
             calculatorProviderBuilder.setProvider(providerName);
         }
-
+        decryptorBuilder.setProvider(providerName);
         return this;
     }
 
@@ -78,7 +80,12 @@ public class JcePBEProtectionRemoverFactory
         {
             return new PGPSecretKeyDecryptorWithAAD(passPhrase, calculatorProvider)
             {
-                public byte[] recoverKeyData(int encAlgorithm, byte[] key, byte[] iv, byte[] aad, byte[] keyData,  int keyOff, int keyLen)
+                @Override
+                public byte[] recoverKeyData(int encAlgorithm, int aeadAlgorithm, byte[] s2kKey, byte[] iv, int packetTag, int keyVersion, byte[] keyData, byte[] pubkeyData) throws PGPException {
+                    return decryptorBuilder.build(passPhrase).recoverKeyData(encAlgorithm, aeadAlgorithm, s2kKey, iv, packetTag, keyVersion, keyData, pubkeyData);
+                }
+
+                public byte[] recoverKeyData(int encAlgorithm, byte[] key, byte[] iv, byte[] aad, byte[] keyData, int keyOff, int keyLen)
                     throws PGPException
                 {
                     try
@@ -109,37 +116,7 @@ public class JcePBEProtectionRemoverFactory
         }
         else
         {
-            return new PBESecretKeyDecryptor(passPhrase, calculatorProvider)
-            {
-                public byte[] recoverKeyData(int encAlgorithm, byte[] key, byte[] iv, byte[] keyData, int keyOff, int keyLen)
-                    throws PGPException
-                {
-                    try
-                    {
-                        Cipher c;
-                        c = helper.createCipher(PGPUtil.getSymmetricCipherName(encAlgorithm) + "/CBC/NoPadding");
-                        c.init(Cipher.DECRYPT_MODE, JcaJcePGPUtil.makeSymmetricKey(encAlgorithm, key), new IvParameterSpec(iv));
-                        return c.doFinal(keyData, keyOff, keyLen);
-                    }
-                    catch (IllegalBlockSizeException e)
-                    {
-                        throw new PGPException("illegal block size: " + e.getMessage(), e);
-                    }
-                    catch (BadPaddingException e)
-                    {
-                        throw new PGPException("bad padding: " + e.getMessage(), e);
-                    }
-                    catch (InvalidAlgorithmParameterException e)
-                    {
-                        throw new PGPException("invalid parameter: " + e.getMessage(), e);
-                    }
-                    catch (InvalidKeyException e)
-                    {
-                        throw new PGPException("invalid key: " + e.getMessage(), e);
-                    }
-                }
-            };
-
+            return decryptorBuilder.build(passPhrase);
         }
     }
 }
