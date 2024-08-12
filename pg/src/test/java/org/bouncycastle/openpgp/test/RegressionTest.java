acb6a8d33135c284def7270fc829c72617bfb0fe
Paul Schaub <vanitasvitae@fsfe.org>
2024-08-12 14:50:31 +0200

    Add test for v6 signature generation

--- pg/src/test/java/org/bouncycastle/openpgp/test/RegressionTest.java
+++ pg/src/test/java/org/bouncycastle/openpgp/test/RegressionTest.java
@@ -80,7 +80,8 @@ public class RegressionTest
 
         new Curve25519PrivateKeyEncodingTest(),
         new EdDSAKeyConversionWithLeadingZeroTest(),
-        new ECDSAKeyPairTest()
+        new ECDSAKeyPairTest(),
+        new PGPv6SignatureTest()
     };
 
     public static void main(String[] args)
