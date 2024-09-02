6c39061dea33118262a8a0c0ddf26e8a96ca7f6d
Paul Schaub <vanitasvitae@fsfe.org>
2024-09-02 15:54:58 +0200

    Secret Key decryption: Require AEAD for Argon2

--- pg/src/main/java/org/bouncycastle/openpgp/PGPSecretKey.java
+++ pg/src/main/java/org/bouncycastle/openpgp/PGPSecretKey.java
@@ -545,6 +545,15 @@ public class PGPSecretKey
 
         if (secret.getEncAlgorithm() != SymmetricKeyAlgorithmTags.NULL)
         {
+            if (secret.getS2K().getType() == S2K.ARGON_2 && secret.getS2KUsage() != SecretKeyPacket.USAGE_AEAD)
+            {
+                throw new PGPException("Argon2 MUST be used with AEAD.");
+            }
+            if (getPublicKey().getVersion() == PublicKeyPacket.VERSION_6 && secret.getS2K().getType() == S2K.SIMPLE)
+            {
+                throw new PGPException("Version 6 keys MUST NOT use Simple S2K.");
+            }
+
             try
             {
                 byte[] key = decryptorFactory.makeKeyFromPassPhrase(secret.getEncAlgorithm(), secret.getS2K());
