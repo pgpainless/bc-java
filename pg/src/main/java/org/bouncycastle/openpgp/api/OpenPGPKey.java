a39a67374d22ab72d8605968b35c8546d0b8a946
Paul Schaub <vanitasvitae@fsfe.org>
2024-11-14 14:58:51 +0100

    OpenPGPKey: overwrite getPGPKeyRing()

--- pg/src/main/java/org/bouncycastle/openpgp/api/OpenPGPKey.java
+++ pg/src/main/java/org/bouncycastle/openpgp/api/OpenPGPKey.java
@@ -6,17 +6,16 @@ import org.bouncycastle.openpgp.operator.PGPContentVerifierBuilderProvider;
 public class OpenPGPKey
         extends OpenPGPCertificate
 {
-    private final PGPSecretKeyRing rawKey;
 
     public OpenPGPKey(PGPSecretKeyRing rawKey,
                       PGPContentVerifierBuilderProvider contentVerifierBuilderProvider)
     {
         super(rawKey, contentVerifierBuilderProvider);
-        this.rawKey = rawKey;
     }
 
-    public PGPSecretKeyRing getRawKey()
+    @Override
+    public PGPSecretKeyRing getPGPKeyRing()
     {
-        return rawKey;
+        return (PGPSecretKeyRing) super.getPGPKeyRing();
     }
 }
